4fd68ececf62c5c16367345f1ce6cd47aefb61aa
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-09-02 13:31:32 +0200

    Imported libs

--- ImageRenderer.cpp
+++ ImageRenderer.cpp
@@ -1,8 +1,13 @@
 #include "ImageRenderer.h"
+#include <citro2d.h>
+#include <string>
+#include <vector>
+
+using namespace std;
 
 ImageRenderer::ImageRenderer(){
-
+    
 }
 ImageRenderer::~ImageRenderer(){
-    
+
 }
--- ImageRenderer.h
+++ ImageRenderer.h
@@ -1,4 +1,9 @@
 #pragma once
+#include <citro2d.h>
+#include <string>
+#include <vector>
+
+using namespace std;
 
 class ImageRenderer{
     public:
