48f5ae100f6a5db4088337c2c8b5c1970f94afaf
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-09-03 17:34:22 +0200

    Cleared OpusAudioPlayer because I can't make it work no matter what

--- OpusAudioPlayer.h
+++ OpusAudioPlayer.h
@@ -1,31 +1,10 @@
-#ifndef OPUS_AUDIO_PLAYER_H
-#define OPUS_AUDIO_PLAYER_H
-
-#include <string>
-#include <opusfile.h>
 #include <3ds.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <cstring>
-#include <math.h>
-
-#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
+#include <opusfile.h>
 
 class OpusAudioPlayer {
 public:
     OpusAudioPlayer();
     ~OpusAudioPlayer();
-
-    bool load(char* filePath);
-    bool audioInit();
-
 private:
-    void audioCallback(void *const nul_);
-    void audioThread(void *const opusFile_);
-    bool fillBuffer(OggOpusFile *opusFile_, ndspWaveBuf *waveBuf_);
-    void audioExit();
-    void audioCallback(void *const nul_);
-    void audioThread(void *const opusFile_);
-};
-
-#endif
+    OggOpusFile* opusFile;
+}
