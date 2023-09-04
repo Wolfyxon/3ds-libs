aaad06fe63ccd7153be4707486b3b48a9194074e
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-09-04 16:23:32 +0200

    Initial OpusAudioPlayer

--- OpusAudioPlayer.cpp
+++ OpusAudioPlayer.cpp
@@ -1,13 +1,19 @@
 #include "OpusAudioPlayer.h"
-#include <3ds.h>
-#include <cstdio>
-#include <cstdlib>
-#include <thread>
 
-OpusAudioPlayer::OpusAudioPlayer(){
-
+OpusAudioPlayer::OpusAudioPlayer(int channel_) {
+    channel = channel_;
 }
 
 OpusAudioPlayer::~OpusAudioPlayer() {
-   
+    unload();
+}
+
+bool OpusAudioPlayer::load(char* filePath){
+    int error = 0;
+    opusFile = op_open_file(filePath, &error);
+    return !(error!=0 || opusFile == NULL);
+}
+
+void OpusAudioPlayer::unload(){
+    op_free(opusFile);
 }
--- OpusAudioPlayer.h
+++ OpusAudioPlayer.h
@@ -1,10 +1,21 @@
 #include <3ds.h>
 #include <opusfile.h>
+#include <cstring>
+
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
 
 class OpusAudioPlayer {
 public:
-    OpusAudioPlayer();
+    OpusAudioPlayer(int channel_=0);
     ~OpusAudioPlayer();
+
+    int channel = 0;
+    OggOpusFile *opusFile = NULL;
+
+    bool load(char* filePath);
+    void unload();
+    bool play();
+
 private:
-    OggOpusFile* opusFile;
+
 };
