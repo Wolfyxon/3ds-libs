5d7907d97cc3ad68ddbd32c59fa98e6db3e45156
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-09-04 22:56:19 +0200

    OpusAudioPlayer progress

--- OpusAudioPlayer.cpp
+++ OpusAudioPlayer.cpp
@@ -11,9 +11,41 @@ OpusAudioPlayer::~OpusAudioPlayer() {
 bool OpusAudioPlayer::load(char* filePath){
     int error = 0;
     opusFile = op_open_file(filePath, &error);
-    return !(error!=0 || opusFile == NULL);
+    if(error != 0) return false;
+    if(opusFile == NULL) return false;
+
+    return true;
 }
 
 void OpusAudioPlayer::unload(){
     op_free(opusFile);
+}
+
+bool OpusAudioPlayer::audioInit() {
+    ndspChnReset(channel);
+    ndspSetOutputMode(NDSP_OUTPUT_STEREO);
+    ndspChnSetInterp(channel, NDSP_INTERP_POLYPHASE);
+    ndspChnSetRate(channel, SAMPLE_RATE);
+    ndspChnSetFormat(channel, NDSP_FORMAT_STEREO_PCM16);
+
+    const size_t bufferSize = WAVEBUF_SIZE * ARRAY_SIZE(s_waveBufs);
+    s_audioBuffer = (int16_t *)linearAlloc(bufferSize);
+    if(!s_audioBuffer) {return false;}
+
+    memset(&s_waveBufs, 0, sizeof(s_waveBufs));
+    int16_t *buffer = s_audioBuffer;
+
+    for(size_t i = 0; i < ARRAY_SIZE(s_waveBufs); ++i) {
+        s_waveBufs[i].data_vaddr = buffer;
+        s_waveBufs[i].status     = NDSP_WBUF_DONE;
+
+        buffer += WAVEBUF_SIZE / sizeof(buffer[0]);
+    }
+
+    return true;
+}
+
+bool OpusAudioPlayer::play(){
+    if(opusFile == NULL) return false;
+
 }
--- OpusAudioPlayer.h
+++ OpusAudioPlayer.h
@@ -17,5 +17,21 @@ public:
     bool play();
 
 private:
+    static const int SAMPLE_RATE = 48000;            // Opus is fixed at 48kHz
+    static const int SAMPLES_PER_BUF = SAMPLE_RATE * 120 / 1000;  // 120ms buffer
+    static const int CHANNELS_PER_SAMPLE = 2;        // We ask libopusfile for
+                                                 // stereo output; it will down
+                                                 // -mix for us as necessary.
 
+    static const int THREAD_AFFINITY = -1;           // Execute thread on any core
+    static const int THREAD_STACK_SZ = 32 * 1024;    // 32kB stack for audio thread
+
+    static const size_t WAVEBUF_SIZE = SAMPLES_PER_BUF * CHANNELS_PER_SAMPLE
+    * sizeof(int16_t);                           // Size of NDSP wavebufs
+
+
+    ndspWaveBuf s_waveBufs[3];
+    int16_t *s_audioBuffer = NULL;
+
+    bool audioInit();
 };
