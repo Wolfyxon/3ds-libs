18a3317d0813b0195799cf2f4d985148988e979f
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-09-05 19:17:44 +0200

    Placeholder size functions

--- renderable/Sprite.h
+++ renderable/Sprite.h
@@ -5,5 +5,33 @@ class Sprite : Renderable {
         Sprite();
         ~Sprite();
 
+        unsigned int scaleX = 1;
+        unsigned int scaleY = 1;
+
+        /** Returns the width in pixels. */
+        unsigned int getWidth();
+        /** Returns the height in pixels. */
+        unsigned int getHeight();
+        /**
+         * Calculates the X scale to match the specified pixel width
+         * @param width Width in pixels.
+        */
+        unsigned int setWidth(unsigned int width);
+        /**
+         * Calculates the Y scale to match the specified pixel height and sets it.
+         * @param height Height in pixels.
+        */
+        unsigned int setHeight(unsigned int height);
+        /**
+         * Calculates the X and Y scale from the specified pixel with and height and sets it.
+         * @param width Width in pixels.
+         * @param height Height in pixels.
+        */
+        unsigned int setSize(unsigned int width, unsigned int height);
+
+
+    private:
+        unsigned int width = 0;
+        unsigned int height = 0;
 
 };
