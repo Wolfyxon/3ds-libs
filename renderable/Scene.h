2c5d206b8ec0cc8f0993e06426105edce8466f71
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-09-06 22:51:34 +0200

    Scene now implements TreeElement

--- renderable/Scene.h
+++ renderable/Scene.h
@@ -1,8 +1,9 @@
 #include <citro2d.h>
 #include <3ds.h>
 #include "Renderable.h"
+#include "TreeElement.h"
 
-class Scene : Renderable {
+class Scene : Renderable, TreeElement {
     public:
         Scene(u32 backgroundColor_ = C2D_Color32f(0.0f,0.0f,0.0f,1.0f));
         ~Scene();
