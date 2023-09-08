f58afe75160ab0ba781b41431888dd2de73844b0
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-09-08 09:12:18 +0200

    Scene rendering logic

--- source/renderable/Scene.cpp
+++ source/renderable/Scene.cpp
@@ -4,3 +4,20 @@ Scene::Scene(gfxScreen_t screen, u32 backgroundColor_){
     C2D_CreateScreenTarget(&renderTarget, GFX_LEFT);
     backgroundColor = backgroundColor_;
 }
+
+void Scene::render(){
+    C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
+    C2D_TargetClear(&renderTarget, backgroundColor);
+    C2D_SceneBegin(&renderTarget);
+
+    vector<TreeElement*> descendants = getDescendants();
+    for(size_t i=0; i<descendants.size();i++){
+        TreeElement* desc = descendants[i];
+        if(dynamic_cast<Renderable*>(desc) != nullptr){
+            Renderable* rend = (Renderable*)desc;
+            rend->render();
+        }
+    }
+    descendants.clear();
+    C3D_FrameEnd(0);
+}
