155097f7afe41f27aac3e035e59b5c4a55d69c9c
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-09-08 09:03:41 +0200

    Made render() virtual

--- include/renderable/Renderable.h
+++ include/renderable/Renderable.h
@@ -7,5 +7,5 @@ class Renderable {
         * Renders the renderable object. 
         * @param screen The screen where the object will be rendered.
         */
-        void render(gfxScreen_t screen);
+        virtual void render(gfxScreen_t screen);
 };
