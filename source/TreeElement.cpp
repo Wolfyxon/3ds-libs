c6c4a36f2c6944f5244419465e7e8fc31f37c215
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-09-08 09:29:16 +0200

    addChild() calls setParent() with dontAdd = true

--- source/TreeElement.cpp
+++ source/TreeElement.cpp
@@ -37,7 +37,7 @@ vector<TreeElement*> TreeElement::getAncestors(){
 
 void TreeElement::addChild(TreeElement* child){
     if(child->getParent() == this) return;
-    child->setParent(this);
+    child->setParent(this,false);
     children.push_back(child);
 }
 
