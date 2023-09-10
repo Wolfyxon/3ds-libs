6ed5ffb1eede4717f84701b83f9c32f73ca19361
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-09-10 13:27:44 +0200

    Classified class

---
+++ include/Classified.h
@@ -0,0 +1,23 @@
+#ifndef CLASSIFIED_H
+#define CLASSIFIED_H
+#pragma once
+
+#include <string>
+using std::string;
+
+class Classified {
+    public:
+        /** Returns names of extended classes. */
+        virtual string* getExtended();
+        /** Returns the current class name. */
+        virtual string getType();
+};
+
+/**
+* Checks if a Classified object extends the specified class.
+* @param classified Pointer to the Classified object.
+* @param className Name of the class you want to look for.
+*/
+static bool instanceof(Classified *classified, string className);
+
+#endif
---
+++ source/Classified.cpp
@@ -0,0 +1,20 @@
+#include "../include/Classified.h"
+
+string* Classified::getExtended(){
+    string* ext = new string[1];
+    ext[0] = "Classified";
+    return ext;
+}
+
+string Classified::getType(){
+    return "Classified";
+}
+
+bool instanceof(Classified *classified, string className){
+    if(classified->getType().compare(className)) return true;
+    string* ext = classified->getExtended();
+    for(size_t i=0; i<ext->size(); i++){
+        if(ext[i].compare(className)) return true;
+    }
+    return false;
+}
