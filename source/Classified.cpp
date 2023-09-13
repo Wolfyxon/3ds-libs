418851228d13bc8705ed47744ec6e1a01f1beb88
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-09-13 18:36:55 +0200

    Fixed instanceof always returning true

--- source/Classified.cpp
+++ source/Classified.cpp
@@ -10,10 +10,10 @@ string Classified::getType(){
 }
 
 bool instanceof(Classified *classified, string className){
-    if(classified->getType().compare(className)) return true;
+    if(classified->getType() == className) return true;
     string* ext = classified->getExtended();
     for(size_t i=0; i<ext->size(); i++){
-        if(ext[i].compare(className)) return true;
+        if(ext[i] == className) return true;
     }
     return false;
 }
