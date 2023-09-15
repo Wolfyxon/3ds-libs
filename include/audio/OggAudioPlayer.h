d74878a4bae011e0ddc6534c8cf413ffc7ab7204
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-09-15 08:45:15 +0200

    Implemented play() and load() from AudioPlayerBase

--- include/audio/OggAudioPlayer.h
+++ include/audio/OggAudioPlayer.h
@@ -6,4 +6,11 @@ class OggAudioPlayer : public AudioPlayerBase {
     OggAudioPlayer() = default;
     virtual ~OggAudioPlayer() = default;
     
+    /** Plays the audio */
+    virtual void play() = 0;
+    /** 
+    * Loads OGG vorbis audio from the specified file path.
+    * @param filePath Path to the .ogg audio file.
+    */
+    virtual AudioResult load(string filePath) = 0;
 };
