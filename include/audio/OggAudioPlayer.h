f1d21abbc166db0e73c2eecfd86b106dd34d3035
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-09-15 08:51:40 +0200

    Moved all methods and vars into public:

--- include/audio/OggAudioPlayer.h
+++ include/audio/OggAudioPlayer.h
@@ -1,18 +1,19 @@
 #include "AudioPlayerBase.h"
-#include <tremor/ivorbisfile.h>
 #include <ogg/ogg.h>
+#include <tremor/ivorbisfile.h>
 
 class OggAudioPlayer : public AudioPlayerBase {
-    OggAudioPlayer() = default;
-    virtual ~OggAudioPlayer() = default;
-    
-    OggVorbis_File ovf;
+    public:
+        OggAudioPlayer() = default;
+        virtual ~OggAudioPlayer() = default;
 
-    /** Plays the audio */
-    virtual void play() = 0;
-    /** 
-    * Loads OGG vorbis audio from the specified file path.
-    * @param filePath Path to the .ogg audio file.
-    */
-    virtual AudioResult load(string filePath) = 0;
+        OggVorbis_File ovf;
+
+        /** Plays the audio */
+        virtual void play() = 0;
+        /** 
+        * Loads OGG vorbis audio from the specified file path.
+        * @param filePath Path to the .ogg audio file.
+        */
+        virtual AudioResult load(string filePath) = 0;
 };
